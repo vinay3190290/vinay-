d2435ea97f665c7dffd7ee3491666f97df791d19
vinay3190290 <124114684+vinay3190290@users.noreply.github.com>
2023-08-07 10:09:14 +0530

    Update Location

--- Location Merge.cpp
+++ Location Merge.cpp
@@ -1,35 +1,20 @@
-// C program for Merge Sort
 #include <stdio.h>
-#include <stdlib.h>
-
-// Merges two subarrays of arr[].
-// First subarray is arr[l..m]
-// Second subarray is arr[m+1..r]
 void merge(int arr[], int l, int m, int r)
 {
 	int i, j, k;
 	int n1 = m - l + 1;
 	int n2 = r - m;
 
-	// Create temp arrays
 	int L[n1], R[n2];
 
-	// Copy data to temp arrays
-	// L[] and R[]
 	for (i = 0; i < n1; i++)
 		L[i] = arr[l + i];
 	for (j = 0; j < n2; j++)
 		R[j] = arr[m + 1 + j];
-
-	// Merge the temp arrays back
-	// into arr[l..r]
-	// Initial index of first subarray
 	i = 0;
 
-	// Initial index of second subarray
 	j = 0;
 
-	// Initial index of merged subarray
 	k = l;
 	while (i < n1 && j < n2) {
 		if (L[i] <= R[j]) {
@@ -42,44 +27,29 @@ void merge(int arr[], int l, int m, int r)
 		}
 		k++;
 	}
-
-	// Copy the remaining elements
-	// of L[], if there are any
 	while (i < n1) {
 		arr[k] = L[i];
 		i++;
 		k++;
 	}
-
-	// Copy the remaining elements of
-	// R[], if there are any
 	while (j < n2) {
 		arr[k] = R[j];
 		j++;
 		k++;
 	}
 }
-
-// l is for left index and r is
-// right index of the sub-array
-// of arr to be sorted
 void mergeSort(int arr[], int l, int r)
 {
 	if (l < r) {
-		// Same as (l+r)/2, but avoids
-		// overflow for large l and h
+		
 		int m = l + (r - l) / 2;
 
-		// Sort first and second halves
 		mergeSort(arr, l, m);
 		mergeSort(arr, m + 1, r);
 
 		merge(arr, l, m, r);
 	}
 }
-
-// UTILITY FUNCTIONS
-// Function to print an array
 void printArray(int A[], int size)
 {
 	int i;
